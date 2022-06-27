11e799ff9094641570d94ecd8b1c333cd27a15b7
BekirKagan <kagankaraahmetoglu@hotmail.com>
2022-06-27 12:02:07 +0300

    First commit

---
+++ MoreSlots/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace MoreSlots
+{
+    partial class MainWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectGameComboBox = new System.Windows.Forms.ComboBox();
+            this.deleteGameButton = new System.Windows.Forms.Button();
+            this.gameControlPanel = new System.Windows.Forms.Panel();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.addGameButton = new System.Windows.Forms.Button();
+            this.slotControlPanel = new System.Windows.Forms.Panel();
+            this.loadSlotButton = new System.Windows.Forms.Button();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.saveSlotButton = new System.Windows.Forms.Button();
+            this.slotSelectionPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.clearSlotButton = new System.Windows.Forms.Button();
+            this.gameControlPanel.SuspendLayout();
+            this.slotControlPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // selectGameComboBox
+            // 
+            this.selectGameComboBox.FormattingEnabled = true;
+            this.selectGameComboBox.Location = new System.Drawing.Point(9, 3);
+            this.selectGameComboBox.Name = "selectGameComboBox";
+            this.selectGameComboBox.Size = new System.Drawing.Size(242, 21);
+            this.selectGameComboBox.TabIndex = 0;
+            this.selectGameComboBox.SelectionChangeCommitted += new System.EventHandler(this.selectGameComboBox_SelectionChangeCommitted);
+            // 
+            // deleteGameButton
+            // 
+            this.deleteGameButton.Location = new System.Drawing.Point(197, 61);
+            this.deleteGameButton.Name = "deleteGameButton";
+            this.deleteGameButton.Size = new System.Drawing.Size(54, 21);
+            this.deleteGameButton.TabIndex = 1;
+            this.deleteGameButton.Text = "Delete";
+            this.deleteGameButton.UseVisualStyleBackColor = true;
+            this.deleteGameButton.Click += new System.EventHandler(this.deleteGameButton_Click);
+            // 
+            // gameControlPanel
+            // 
+            this.gameControlPanel.Controls.Add(this.nameLabel);
+            this.gameControlPanel.Controls.Add(this.pathLabel);
+            this.gameControlPanel.Controls.Add(this.nameTextBox);
+            this.gameControlPanel.Controls.Add(this.pathTextBox);
+            this.gameControlPanel.Controls.Add(this.deleteGameButton);
+            this.gameControlPanel.Controls.Add(this.addGameButton);
+            this.gameControlPanel.Location = new System.Drawing.Point(3, 6);
+            this.gameControlPanel.Name = "gameControlPanel";
+            this.gameControlPanel.Size = new System.Drawing.Size(258, 90);
+            this.gameControlPanel.TabIndex = 2;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(3, 12);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(38, 13);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Name:";
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(9, 38);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(32, 13);
+            this.pathLabel.TabIndex = 3;
+            this.pathLabel.Text = "Path:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(44, 9);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(207, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(44, 35);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(207, 20);
+            this.pathTextBox.TabIndex = 2;
+            // 
+            // addGameButton
+            // 
+            this.addGameButton.Location = new System.Drawing.Point(137, 61);
+            this.addGameButton.Name = "addGameButton";
+            this.addGameButton.Size = new System.Drawing.Size(54, 21);
+            this.addGameButton.TabIndex = 1;
+            this.addGameButton.Text = "Add";
+            this.addGameButton.UseVisualStyleBackColor = true;
+            this.addGameButton.Click += new System.EventHandler(this.addGameButton_Click);
+            // 
+            // slotControlPanel
+            // 
+            this.slotControlPanel.Controls.Add(this.clearSlotButton);
+            this.slotControlPanel.Controls.Add(this.loadSlotButton);
+            this.slotControlPanel.Controls.Add(this.infoLabel);
+            this.slotControlPanel.Controls.Add(this.saveSlotButton);
+            this.slotControlPanel.Controls.Add(this.selectGameComboBox);
+            this.slotControlPanel.Controls.Add(this.slotSelectionPanel);
+            this.slotControlPanel.Location = new System.Drawing.Point(3, 102);
+            this.slotControlPanel.Name = "slotControlPanel";
+            this.slotControlPanel.Size = new System.Drawing.Size(258, 236);
+            this.slotControlPanel.TabIndex = 3;
+            // 
+            // loadSlotButton
+            // 
+            this.loadSlotButton.Location = new System.Drawing.Point(197, 180);
+            this.loadSlotButton.Name = "loadSlotButton";
+            this.loadSlotButton.Size = new System.Drawing.Size(54, 21);
+            this.loadSlotButton.TabIndex = 1;
+            this.loadSlotButton.Text = "Load";
+            this.loadSlotButton.UseVisualStyleBackColor = true;
+            this.loadSlotButton.Click += new System.EventHandler(this.loadSlotButton_Click);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.infoLabel.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.infoLabel.Location = new System.Drawing.Point(197, 30);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(54, 120);
+            this.infoLabel.TabIndex = 2;
+            this.infoLabel.Text = "Select a game and then select a save you want to load.";
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // saveSlotButton
+            // 
+            this.saveSlotButton.Location = new System.Drawing.Point(197, 153);
+            this.saveSlotButton.Name = "saveSlotButton";
+            this.saveSlotButton.Size = new System.Drawing.Size(54, 21);
+            this.saveSlotButton.TabIndex = 1;
+            this.saveSlotButton.Text = "Save";
+            this.saveSlotButton.UseVisualStyleBackColor = true;
+            this.saveSlotButton.Click += new System.EventHandler(this.saveSlotButton_Click);
+            // 
+            // slotSelectionPanel
+            // 
+            this.slotSelectionPanel.AutoScroll = true;
+            this.slotSelectionPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.slotSelectionPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.slotSelectionPanel.Location = new System.Drawing.Point(9, 30);
+            this.slotSelectionPanel.Name = "slotSelectionPanel";
+            this.slotSelectionPanel.Size = new System.Drawing.Size(182, 198);
+            this.slotSelectionPanel.TabIndex = 0;
+            this.slotSelectionPanel.WrapContents = false;
+            // 
+            // clearSlotButton
+            // 
+            this.clearSlotButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.clearSlotButton.Location = new System.Drawing.Point(197, 207);
+            this.clearSlotButton.Name = "clearSlotButton";
+            this.clearSlotButton.Size = new System.Drawing.Size(54, 21);
+            this.clearSlotButton.TabIndex = 1;
+            this.clearSlotButton.Text = "Clear";
+            this.clearSlotButton.UseVisualStyleBackColor = true;
+            this.clearSlotButton.Click += new System.EventHandler(this.clearSlotButton_Click);
+            // 
+            // MainWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 345);
+            this.Controls.Add(this.slotControlPanel);
+            this.Controls.Add(this.gameControlPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainWin";
+            this.Text = "MoreSlots";
+            this.Load += new System.EventHandler(this.MainWin_Load);
+            this.gameControlPanel.ResumeLayout(false);
+            this.gameControlPanel.PerformLayout();
+            this.slotControlPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox selectGameComboBox;
+        private System.Windows.Forms.Button deleteGameButton;
+        private System.Windows.Forms.Panel gameControlPanel;
+        private System.Windows.Forms.Panel slotControlPanel;
+        private System.Windows.Forms.Button addGameButton;
+        private System.Windows.Forms.Button saveSlotButton;
+        private System.Windows.Forms.FlowLayoutPanel slotSelectionPanel;
+        private System.Windows.Forms.Button loadSlotButton;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button clearSlotButton;
+    }
+}
+
---
+++ MoreSlots/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MoreSlots
+{
+    public partial class MainWin : Form
+    {
+        Dictionary<string, string> games = new Dictionary<string, string>();
+        Button lastClickedSlot = null;
+        const string gamesFile = "existing_games.txt";
+        const int maxSlotCount = 10;
+
+        public MainWin()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWin_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(gamesFile) == false)
+                File.Create(gamesFile).Close();
+
+            using (StreamReader file = new StreamReader(gamesFile))
+            {
+                int index = 0;
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    string gameName = line.Split(',')[0];
+                    string gamePath = line.Split(',')[1];
+
+                    selectGameComboBox.Items.Add(gameName);
+                    games.Add(gameName, gamePath);
+                    index++;
+                }
+            }
+        }
+
+        private void addGameButton_Click(object sender, EventArgs e)
+        {
+            string gameName = nameTextBox.Text;
+            string gamePath = pathTextBox.Text;
+            
+            //If either one of the textboxes are empty, return.
+            if (gameName == string.Empty || gamePath == string.Empty)
+            {
+                Warn("Either one of the textboxes are empty!");
+                return;
+            }
+
+            //Add the game to combobox and dictionary.
+            selectGameComboBox.Items.Add(gameName);
+            games.Add(gameName, gamePath);
+            if (File.ReadAllLines(gamesFile).Contains(gameName) == false)
+                File.AppendAllText(gamesFile, gameName + "," + gamePath);
+
+            //Create a folder for the game slots if not already exists.
+            if (Directory.Exists(gameName) == false)
+                Directory.CreateDirectory(gameName);
+
+            //Clear textboxes.
+            nameTextBox.Text = string.Empty;
+            pathTextBox.Text = string.Empty;
+        }
+
+        private void deleteGameButton_Click(object sender, EventArgs e)
+        {
+            string gameName = nameTextBox.Text;
+
+            //If the name textbox is empty or the name doesn't exist in combobox, return.
+            if (gameName == string.Empty || selectGameComboBox.Items.Contains(gameName) == false)
+            {
+                Warn("Name textbox is empty or the name doesn't exist in combobox!");
+                return;
+            }
+
+            //Remove the game from combobox and the dictionary.
+            selectGameComboBox.Items.Remove(gameName);
+            games.Remove(gameName);
+
+            string tempFile = Path.GetTempFileName();
+
+            using (var sr = new StreamReader(gamesFile))
+            using (var sw = new StreamWriter(tempFile))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Contains(gameName) == false)
+                        sw.WriteLine(line);
+                }
+            }
+
+            File.Delete(gamesFile);
+            File.Move(tempFile, gamesFile);
+
+            //Clear the name textbox and combobox.
+            nameTextBox.Text = string.Empty;
+            selectGameComboBox.Text = string.Empty;
+
+            //Delete the game folder and it's slots.
+            ClearSlots();
+            DeleteDirectory(gameName);
+        }
+
+        private void selectGameComboBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            //Clear old previous game's slots first.
+            ClearSlots();
+
+            //Create slots for the game.
+            CreateSlots(maxSlotCount);
+        }
+
+        private void slotButton_Click(object sender, EventArgs e)
+        {
+            //Store the last clicked button.
+            lastClickedSlot = (Button) sender;
+        }
+
+        private void saveSlotButton_Click(object sender, EventArgs e)
+        {
+            //If no game selected or the game doesn't exist in dictionary, return.
+            if (selectGameComboBox.Text == string.Empty || games.ContainsKey(selectGameComboBox.Text) == false)
+            {
+                Warn("No game selected or the game doesn't exist in dictionary!");
+                return;
+            }
+
+            //Store the game name and it's path.
+            string selectedGame = selectGameComboBox.Text;
+            string selectedGamePath = games[selectedGame];
+
+            //If no slot is clicked, return.
+            if (lastClickedSlot == null)
+            {
+                Warn("You didn't clicked a slot!");
+                return;
+            }
+
+            //If selected game's directory is empty, return.
+            if (Directory.GetFiles(selectedGamePath) == null)
+            {
+                Warn("Selected game's directory is empty!");
+                return;
+            }
+
+            //Move current save files to the last clicked slot.
+            foreach (string file in Directory.GetFiles(selectedGamePath))
+            {
+                if (!File.Exists(selectedGame + "\\" + lastClickedSlot.Text + "\\" + Path.GetFileName(file)))
+                    File.Move(file, selectedGame + "\\" + lastClickedSlot.Text + "\\" + Path.GetFileName(file));
+                else
+                    File.Delete(file);
+            }
+        }
+
+        private void loadSlotButton_Click(object sender, EventArgs e)
+        {
+            //If no game selected or the game doesn't exist in dictionary, return.
+            if (selectGameComboBox.Text == string.Empty || games.ContainsKey(selectGameComboBox.Text) == false)
+            {
+                Warn("No game selected or the game doesn't exist in dictionary!");
+                return;
+            }
+
+            //Store the game name and slot's path.
+            string selectedGame = selectGameComboBox.Text;
+            string selectedGamePath = games[selectedGame];
+            string slotPath = selectedGame + "\\" + lastClickedSlot.Text;
+
+            //If no slot is clicked, return.
+            if (lastClickedSlot == null)
+            {
+                Warn("You didn't clicked a slot!");
+                return;
+            }
+
+            //If slot is empty, return.
+            if (Directory.GetFiles(slotPath) == null)
+            {
+                Warn("Selected game's directory is empty!");
+                return;
+            }
+
+            //Copy the files in last clicked slot to selected game's directory.
+            foreach (string file in Directory.GetFiles(slotPath))
+            {
+                if (!File.Exists(selectedGamePath + "\\" + Path.GetFileName(file)))
+                    File.Copy(file, selectedGamePath + "\\" + Path.GetFileName(file));
+            }
+        }
+        
+        private void clearSlotButton_Click(object sender, EventArgs e)
+        {
+            //Store the game name and slot's path.
+            string selectedGame = selectGameComboBox.Text;
+            string slotPath = selectedGame + "\\" + lastClickedSlot.Text;
+
+            //If the slot is empty, return.
+            if (Directory.GetFiles(slotPath) == null)
+            {
+                Warn("Slot is empty!");
+                return;
+            }
+
+            //Delete all files in the slot.
+            foreach (string file in Directory.GetFiles(slotPath))
+            {
+                File.Delete(file);
+            }
+        }
+
+        private void ClearSlots()
+        {
+            //Clear slots.
+            slotSelectionPanel.Controls.Clear();
+        }
+
+        private void CreateSlots(int maxSize)
+        {
+            string gameName = (string) selectGameComboBox.SelectedItem;
+            string gamePath = games[gameName];
+
+            //Create slots with the size of maxSize.
+            for (int i = 0; i < maxSize; i++)
+            {
+                Button slot = new Button();
+                slot.Width = slotSelectionPanel.Width - 30;
+                slot.FlatStyle = FlatStyle.Flat;
+                slot.Text = "Slot" + (i + 1);
+                slot.Click += slotButton_Click;
+                slotSelectionPanel.Controls.Add(slot);
+
+                string slotPath = gameName + "\\" + slot.Text;
+
+                //Create slot folders in the game folder if they don't already exist.
+                if (Directory.Exists(slotPath) == false)
+                    Directory.CreateDirectory(slotPath);
+            }
+        }
+
+        private void FindExistingGames()
+        {
+            //If there are no existing games, return.
+            if (Directory.GetFiles(Directory.GetCurrentDirectory()) == null)
+            {
+                Warn("Couldn't find any existing game folders!");
+                return;
+            }
+
+            //Add existing games to the list.
+            foreach (string game in Directory.GetFiles(Directory.GetCurrentDirectory()))
+            {
+                
+            }
+        }
+
+        private void DeleteDirectory(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            string[] dirs = Directory.GetDirectories(path);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(path, false);
+        }
+
+        private void Warn(string warning)
+        {
+            Console.WriteLine("Warning: " + warning);
+        }
+    }
+}
